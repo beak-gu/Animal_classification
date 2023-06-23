750795cf0cb47fde353c55c8d8dda7552031529e
beak-gu <ngw77@naver.com>
2023-06-23 17:45:55 +0900

    asd

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import json
 from PIL import Image
 import PIL.Image as pilimg
+from tensorboard import summary
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -19,13 +20,15 @@ from sklearn.metrics import f1_score
 from tqdm import tqdm
 from torchvision import transforms, datasets
 from torch.utils.data import Subset, dataloader
+from efficient_net import *
 
 
 ## parameter
 is_Test = False
 # is_Test = True
 num_epochs = 25
-batch_size = 128
+# batch_size가 클수록 좋기는 한데,,, 일단 컴터가 안좋으니까 64 랩실컴으로 돌릴때는 128으로 하자
+batch_size = 64
 
 data_path = r"C:\Users\ngw77\Desktop\Ncloud\Image_Training\Dataset"
 save_path = r"C:\Users\ngw77\Desktop\Ncloud\Image_Training\output"
@@ -38,20 +41,26 @@ data_test_path = os.path.join(data_path, "test")
 transform_function = transforms.Compose(
     [
         transforms.Resize((224, 224)),  # 모델 입력사이즈로 resize
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        transforms.ToTensor(),  # 0101로 바꾸기
+        # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ]
 )
+
 
 dataset = {}
 dataset["train"] = datasets.ImageFolder(data_train_path, transform_function)
 dataset["valid"] = datasets.ImageFolder(data_valid_path, transform_function)
 dataset["test"] = datasets.ImageFolder(data_test_path, transform_function)
 
+print(dataset["train"])
+print("////////")
+
 print(
     "data proportion(train:valid:test) = %s : %s : %s"
     % (len(dataset["train"]), len(dataset["valid"]), len(dataset["test"]))
 )
+print("////////")
+
 
 ## data loader 선언
 # gpu있을 시에는 4*gpu갯수 => num_workers=4
@@ -66,6 +75,7 @@ dataloaders["test"] = torch.utils.data.DataLoader(
     dataset["test"], batch_size=batch_size, shuffle=False
 )
 
+
 batch_num["train"], batch_num["valid"], batch_num["test"] = (
     len(dataloaders["train"]),
     len(dataloaders["valid"]),
@@ -76,6 +86,7 @@ print(
     "batch_size : %d,  number of batch(train/valid/test) : %d / %d / %d"
     % (batch_size, batch_num["train"], batch_num["valid"], batch_num["test"])
 )
+print("////////")
 
 
 def imshow(inp, title=None):
@@ -89,7 +100,147 @@ def imshow(inp, title=None):
         plt.title(title)
     plt.pause(0.001)
     # pause a bit so that plots are updated
-    
+
     num_show_img = 8
-    
-    class_names = 
+
+    class_names = {"0": "boar", "1": "elf"}
+
+    input, classes = next(iter(dataloader["train"]))
+    out = torchvision.utils.make_grid(input[:num_show_img])
+    imshow(out, title=[class_names[str(int(x))] for x in classes[:num_show_img]])
+
+    input, classes = next(iter(dataloader["valid"]))
+    out = torchvision.utils.make_grid(input[:num_show_img])
+    imshow(out, title=[class_names[str(int(x))] for x in classes[:num_show_img]])
+
+    input, classes = next(iter(dataloader["test"]))
+    out = torchvision.utils.make_grid(input[:num_show_img])
+    imshow(out, title=[class_names[str(int(x))] for x in classes[:num_show_img]])
+
+
+## batch의 tensor 이미지를 확인하기 위한 함수
+def check_image_from_tensor(check_image, check_class):
+    title = list(check_class.cpu().numpy())  # torch tensor to list
+
+    # 5x1 형식으로 만들기 위해
+    num_image = len(title)
+    if num_image <= 5:
+        columns = num_image
+        rows = 1
+    else:
+        columns = 5
+        rows = int(np.ceil(num_image / columns))
+
+    fig = plt.figure(figsize=(3 * columns, 4 * rows))
+
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+
+    for i in range(1, num_image + 1):
+        inp = check_image[i - 1].numpy().transpose((1, 2, 0))
+        inp = std * inp + mean  # 원본 이미지로 변환
+        inp = np.clip(inp, 0, 1)
+
+        fig.add_subplot(rows, columns, i)
+        plt.title(title[i - 1])
+        plt.imshow(inp)
+    plt.show()
+
+
+num_show_img = 10
+# data check
+inputs, classes = next(iter(dataloaders["train"]))
+print(inputs.size())
+############# inputs의 사이즈 : torch.Size([128, 3, 224, 224]) #####################
+check_image, check_class = inputs[:num_show_img], classes[:num_show_img]
+check_image_from_tensor(check_image, check_class)
+
+######################################################################################################################################
+model = EfficientNet()
+# CrossEntropyLoss 사용
+criterion = nn.CrossEntropyLoss()
+# backpropagation method
+learning_rate = 1e-3
+optimizer = optim.Adam(model.parameters())
+num_epochs = 10
+num_batches = len(dataloaders["train"])
+
+list_epoch = []
+list_train_loss = []
+list_val_loss = []
+list_train_acc = []
+list_val_acc = []
+train_total = 0
+val_total = 0
+train_correct = 0
+val_correct = 0
+for epoch in range(num_epochs):
+    trn_loss = 0.0
+    train_total = 0
+    # val_total= 0
+    train_correct = 0
+    # val_correct = 0
+    for i, data in enumerate(dataloaders["train"]):
+        x, labels = data
+        # grad init
+        optimizer.zero_grad()
+        # forward propagation
+        model_output = model(x)
+        # calculate acc
+        _, predicted = torch.max(model_output.data, 1)
+        train_total += labels.size(0)
+        train_correct += (predicted == labels).sum().item()
+        # calculate loss
+        loss = criterion(model_output, labels)
+        # back propagation
+        loss.backward()
+        # weight update
+        optimizer.step()
+
+        # trn_loss summary
+        trn_loss += loss.item()
+        # del (memory issue)
+        del loss
+        del model_output
+        if (i + 1) % 100 == 0:
+            print(
+                "epoch: {}/{} | batch: {} | trn loss: {:.4f} | trn acc: {:.4f}%".format(
+                    epoch + 1,
+                    num_epochs,
+                    i + 1,
+                    trn_loss / i,
+                    100 * train_correct / train_total,
+                )
+            )
+
+        # 학습과정 출력
+        if (i + 1) % 100 == 0:  # every 100 mini-batches
+            with torch.no_grad():  # very very very very important!!!
+                val_loss = 0.0
+                for j, val in enumerate(valloader):
+                    val_x, val_labels = val
+                    val_output = model(val_x)
+                    # calculate acc
+                    _, predicted = torch.max(val_output.data, 1)
+                    val_total += val_labels.size(0)
+                    val_correct += (predicted == val_labels).sum().item()
+
+                    v_loss = criterion(val_output, val_labels)
+                    val_loss += v_loss
+
+            print(
+                "epoch: {}/{} | batch: {} | trn loss: {:.4f} | trn acc: {:.4f}% | val loss: {:.4f} | val acc: {:.4f}%".format(
+                    epoch + 1,
+                    num_epochs,
+                    i + 1,
+                    trn_loss / i,
+                    100 * train_correct / train_total,
+                    val_loss / len(valloader),
+                    100 * val_correct / val_total,
+                )
+            )
+    list_epoch.append(epoch + 1)
+    list_train_loss.append(trn_loss / num_batches)
+    # list_val_loss.append(val_loss/len(valloader))
+    list_train_acc.append(100 * train_correct / train_total)
+    # list_val_acc.append(100 * val_correct / val_total)
